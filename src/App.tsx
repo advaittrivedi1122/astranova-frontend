944bc3092da812e06ad8da6a42bdef29fd878298
Advait Trivedi <trivediadvait0@gmail.com>
2024-01-10 08:15:18 +0530

    setup tailwind with react typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+import './App.css'
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className='bg-slate-700 h-screen'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+    <h1 className="text-3xl text-blue-600">
+        OpenSea
+    </h1>
+    </>
+  )
+}
+
+export default Navbar
